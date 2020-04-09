cedef6f5592075aa55893a22cd8a1b3e0a334f79
Zach Schroeder <zpsman2017@gmail.com>
2020-04-09 21:19:35 +0000

    Good start to nav bar

--- navbar.php
+++ navbar.php
@@ -1,1 +1,7 @@
-<div id='navbar'>ZPS</div>
+<div class='navbar row'>
+    <div class='col-8'><a>ZPS</a></div>
+    <div class='col-1'><a>Home</a></div>
+    <div class='col-1'><a>School</a></div>
+    <div class='col-1'><a>Current</a></div>
+    <div class='col-1'><a>About</a></div>
+</div>
