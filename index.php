ef5bb0c84700911c03692547db6e3d0a6ed91cbe
Zach Schroeder <zpsman2017@gmail.com>
2020-04-13 17:54:30 +0000

    Slight change to content and hardcode fix to nav

--- index.php
+++ index.php
@@ -16,27 +16,45 @@
 		<div class="col-12 contentDiv">
 			<h1>This is hosted on Zach's personal server!</h1>
 
-			<div class="col-1 filler-top"></div>
-
-			<div class="col-45 headerDiv">
-				<div><h2>Why?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			<div class="row">
+				<div class="col-1 filler-top"></div>
+				<div class="col-45 headerDiv">
+					<h2>Why?</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>	
+				<div class="col-1 filler"></div>
+				<div class="col-45 headerDiv">
+					<h2>How?</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 			</div>
 			
-			<div class="col-1 filler"></div>
 
-			<div class="col-45 headerDiv">
-				<div><h2>How?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			<div class="col-12 filler"></div>
+			
+
+			<div class="row">
+				<div class="col-1 filler-top"></div>
+				<div class="col-10 headerDiv">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				</div>
 			</div>
+
+			<div class="col-12 filler"></div>
+
 		</div>
 
 		
--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 
 <input id="menuButton" onclick="toggleMenu()" type="image" src="/nav.png"/>
 
-<div class='navbar' id='test'>
+<div class='navbar row' id='test'>
     <div class='col-8 navTitle'><a href="/">ZPS</a></div>
     <a href="/"><div class='col-1 navLink'>Home</div></a>
     <a href="/schoolwork"><div class='col-1 navLink'>School</div></a>
