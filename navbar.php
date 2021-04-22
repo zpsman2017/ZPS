b7f2f5682035b6992fc96ee557f9f037b21019f4
Zach Schroeder <zpsman2017@gmail.com>
2021-04-22 13:57:13 +0000

    Added WebAppsFinalProject.png and moved nav.png to images folder

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <?php ?>
 
-<input id="menuButton" onclick="toggleMenu()" type="image" src="/nav.png"/>
+<input id="menuButton" onclick="toggleMenu()" type="image" src="/images/nav.png"/>
 
 <div class='navbar row' id='test'>
     <div class='col-8 navTitle'><a href="/">ZPS</a></div>
