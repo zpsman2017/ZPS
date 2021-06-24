8ebd9801b6d4829a38207c54192f61ebe66d993e
Zach Schroeder <zpsman2017@gmail.com>
2021-06-24 19:19:48 +0000

    Updated current page with recent graduation info and favorite courses

--- current/index.php
+++ current/index.php
@@ -17,7 +17,7 @@
 			<div class="row top">
 				<div class="col-3 filler-top"></div>
 				<div class="col-6 headerDiv">
-					<h1>Still in school, but I've accepted a job offer!</h1>
+					<h1>Recently graduated, soon to start a job</h1>
 				</div>
 			</div>
 
@@ -27,8 +27,9 @@
 					<h2>Miami and Crowe</h2>
 					<div class="col-1 filler-top"></div>
 					<div class="col-10 paraDiv">
-						<p>I am still currently a student at Miami University and plan to graduate in May 2021 with a Bachelor's of Software Engineering.
-							My current coursework includes "Web Services" & my "Senior Design Project" capstone. Starting August 2021, I will be a an IT Assurance Staff Member at Crowe.
+						<p>I recently graduated Miami University in May 2021 with a Bachelor's of Software Engineering.
+							My favorite courses included "Web Services", "Cloud Computing & DevOps", and "Data Abstraction & Structures".
+							Starting August 2021, I will be a an IT Assurance Staff Member at Crowe.
 						</p>
 					</div>
 					<div class="col-6">
